f7da8c3ae42414c7e16f7dbc387a956ee379dde2
Joel Frederico <joelfred@slac.stanford.edu>
2014-05-21 16:16:40 -0700

    Wrong fixed wrong coordinate

--- calibrateCam.m
+++ calibrateCam.m
@@ -1,17 +1,16 @@
-function calibrateCam(pt1, pt2, pt3)
+function calibrateCam(pt1, pt2, pt3,spacing)
 	% maxval=max(max(data.img))
 	% imagesc(data.img,[0,maxval/10])
 
 	delx = double( abs( pt1(1)-pt2(1) ));
 	dely = double( abs( pt1(2)-pt2(2) ));
 
-	theta=atan(dely/(delx*sqrt(2)));
+	theta=atan(dely/(delx*sqrt(2)))
 
-	h = abs( pt1(2)-pt3(2) )
+	h = abs( pt2(2)-pt3(2) )
 
 	w=cos(theta)*h
-
-	res = 2.5*10^3/w
+	res = spacing/w
 
 	display(['Resolution is ' num2str(res) ' um/px.']);
 end
